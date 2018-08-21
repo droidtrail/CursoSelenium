ee00d38b2da8d06c0bcd77ae965be54171c5b95b
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-08-21 20:37:43 -0300

    Refatorando o código. Seção 6. Aula 56

--- CursoSelenium/src/main/java/TesteCadastro.java
+++ CursoSelenium/src/main/java/TesteCadastro.java
@@ -35,12 +35,12 @@ public class TesteCadastro {
 		page.setEsporte("Natacao");
 		page.cadastrar();
 		
-		Assert.assertTrue(page.obterResultadoCadastro().startsWith("Cadastrado!"));
-		Assert.assertTrue(page.obterNomeCadastro().endsWith("Wagner"));
-		Assert.assertEquals("Sobrenome: Costa", page.obterSobrenomeCadastro());
-		Assert.assertEquals("Sexo: Masculino", page.obterSexoCadastro());
-		Assert.assertEquals("Comida: Pizza", page.obterComidaCadastro());
-		Assert.assertEquals("Escolaridade: mestrado", page.obterEscolaridadeCadastro());
-		Assert.assertEquals("Esportes: Natacao", page.obterEsportesCadastro());
+		Assert.assertEquals("Cadastrado!",page.obterResultadoCadastro());
+		Assert.assertEquals("Wagner",page.obterNomeCadastro());
+		Assert.assertEquals("Costa", page.obterSobrenomeCadastro());
+		Assert.assertEquals("Masculino", page.obterSexoCadastro());
+		Assert.assertEquals("Pizza", page.obterComidaCadastro());
+		Assert.assertEquals("mestrado", page.obterEscolaridadeCadastro());
+		Assert.assertEquals("Natacao", page.obterEsportesCadastro());
 	}
 }
