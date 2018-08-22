786a0b0b0e9604e7166acf3d8b0630cf0b42d073
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-08-22 15:41:18 -0300

    Desafio: Combo prime. Seção 6 - Aula 58.

--- CursoSelenium/src/main/java/TestePrime.java
+++ CursoSelenium/src/main/java/TestePrime.java
@@ -16,7 +16,6 @@ public class TestePrime {
 		System.setProperty("webdriver.gecko.driver", "e:\\GeckoDriver\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("https://www.primefaces.org/showcase/ui/input/oneRadio.xhtml");
 		dsl = new DSL(driver);
 	}
 
@@ -27,13 +26,29 @@ public class TestePrime {
 
 	@Test
 	public void deveInteragirComRadioPrime() {
+		driver.get("https://www.primefaces.org/showcase/ui/input/oneRadio.xhtml");
 		//Xbox One
 		dsl.clicarRadio(By.xpath(".//input[@id='j_idt639:console:0']/../..//span"));
 		Assert.assertTrue(dsl.isRadioMarcado("j_idt639:console:0"));
 		//PS4
 		dsl.clicarRadio(By.xpath("//label[.='PS4']/..//span"));
 		Assert.assertTrue(dsl.isRadioMarcado("j_idt639:console:1"));
-		
 	}
+	@Test
+	public void deveInteragirComSelectPrime(){
+		driver.get("https://www.primefaces.org/showcase/ui/input/oneMenu.xhtml");
+		dsl.selecionarComboPrime("j_idt639:console", "PS4");
+		Assert.assertEquals("PS4", dsl.obterTexto("j_idt639:console_label"));
 
+	}
+	
 }
+
+
+
+
+
+
+
+
+
