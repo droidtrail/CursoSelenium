a0247ba0c99226a7fb4254e5bf1af3099b95222e
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-08-24 23:07:09 -0300

    Espera Explícita. Seção 7 - Aula 62.

--- CursoSelenium/src/main/java/TesteSincronismo.java
+++ CursoSelenium/src/main/java/TesteSincronismo.java
@@ -3,9 +3,13 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TesteSincronismo {
 
@@ -37,6 +41,13 @@ public class TesteSincronismo {
 	public void deveInteragirComRespostaDemorada() {
 		
 		dsl.clicarBotao("buttonDelay");
+		dsl.escrever("novoCampo", "Leandro");
+	}
+	@Test
+	public void deveUtilizarEsperaExplicita() {
+		dsl.clicarBotao("buttonDelay");
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("novoCampo")));
 		dsl.escrever("novoCampo", "Leandro");
 	}
 
