f0de81da2c3df3737dcac3ed53de5e3cd9128548
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-08-25 19:53:39 -0300

    Screenshot ao final dos Teste. Seção 8 - Aula 69.

--- CursoSelenium/src/main/java/br/ce/wcaquino/suite/SuiteTeste.java
+++ CursoSelenium/src/main/java/br/ce/wcaquino/suite/SuiteTeste.java
@@ -6,9 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import br.ce.wcaquino.core.DriverFactory;
 import br.ce.wcaquino.test.TesteCadastro;
-import br.ce.wcaquino.test.TesteCampoTreinamento;
 import br.ce.wcaquino.test.TesteRegrasCadastro;
 
 @RunWith(Suite.class)
