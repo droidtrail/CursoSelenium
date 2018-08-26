85cf01919117bd4b4b161d6df0c733beca7775d8
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-08-25 21:02:22 -0300

    Chaveamento de Browser. Seção 8 - Aula 70.

--- CursoSelenium/src/main/java/br/ce/wcaquino/core/DriverFactory.java
+++ CursoSelenium/src/main/java/br/ce/wcaquino/core/DriverFactory.java
@@ -1,7 +1,10 @@
 package br.ce.wcaquino.core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class DriverFactory {
 
@@ -14,8 +17,35 @@ public class DriverFactory {
 	public static WebDriver getDriver() {
 
 		if (driver == null) {
-			System.setProperty("webdriver.gecko.driver", "e:\\GeckoDriver\\geckodriver.exe");
-			driver = new FirefoxDriver();
+			
+			switch (Propriedades.browser) {
+			case FIREFOX:
+				System.setProperty("webdriver.gecko.driver",
+						"e:\\GeckoDriver\\geckodriver.exe");
+				driver = new FirefoxDriver();
+				break;
+			case CHROME: 
+				System.setProperty("webdriver.chrome.driver",
+						"E:\\ChromeDriver_win32\\chromedriver.exe");
+				driver  = new ChromeDriver();
+				break;
+				
+			case INTERNETEXPLORER: 
+				System.setProperty("webdriver.ie.driver",
+						"E:\\ieDriver\\IEDriverServer.exe");
+				driver = new InternetExplorerDriver();
+				break;
+				
+			case EDGE: 
+				System.setProperty("webdriver.edge.driver",
+						"E:\\EdgeDriver\\MicrosoftWebDriver.exe");
+				driver = new EdgeDriver();
+				break;
+		
+			default:
+				break;
+			}
+			
 			driver.manage().window().maximize();
 		}
 		return driver;
--- CursoSelenium/src/main/java/br/ce/wcaquino/core/Propriedades.java
+++ CursoSelenium/src/main/java/br/ce/wcaquino/core/Propriedades.java
@@ -3,5 +3,14 @@ package br.ce.wcaquino.core;
 public class Propriedades {
 	
 	public static boolean FECHAR_BROWSER = false;
-
+	
+	public static Browsers browser = Browsers.INTERNETEXPLORER;
+	
+	public enum Browsers{
+		CHROME,
+		FIREFOX,
+		SAFARI,
+		EDGE,
+		INTERNETEXPLORER
+	}
 }
